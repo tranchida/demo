0550f627f5e6d31903059c128051098bea60ede0
Giampaolo Tranchida <giampaolo@tranchida.ch>
2023-07-08 08:52:45 +0200

    add space

--- src/main/java/ch/tranchida/demo/SampleProcessor.java
+++ src/main/java/ch/tranchida/demo/SampleProcessor.java
@@ -4,15 +4,13 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
-import jakarta.ws.rs.core.Response;
-
 @Component
 public class SampleProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
         
-        exchange.getIn().setBody("{ \"data\": \"Hello" + exchange.getIn().getHeader("data", String.class) + "\"}");
+        exchange.getIn().setBody("{ \"data\": \"Hello " + exchange.getIn().getHeader("data", String.class) + "\"}");
 
     }
     
